aec8ae83845f8998c6c466eca4d851389f2cdae5
naliferopoulos <aliferopoulos@icloud.com>
2020-10-30 09:44:48 -0400

    Added Developer Github link in Preferences

--- infobar/infobarpreferences/IBPRootListController.m
+++ infobar/infobarpreferences/IBPRootListController.m
@@ -10,4 +10,13 @@
 	return _specifiers;
 }
 
+- (void)respring:(id)sender {
+	pid_t pid;
+    const char* args[] = {"killall", "backboardd", NULL};
+    posix_spawn(&pid, "/usr/bin/killall", NULL, NULL, (char* const*)args, NULL);
+}
+
+- (void)openGithub {
+	[[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://github.com/naliferopoulos"]];
+}
 @end
