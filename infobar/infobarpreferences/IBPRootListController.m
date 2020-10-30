a4f0ba08d012cf526920c493b5c4334dd5f1d0b3
naliferopoulos <aliferopoulos@icloud.com>
2020-10-30 09:57:44 -0400

    Added Developer Twitter link in Preferences

--- infobar/infobarpreferences/IBPRootListController.m
+++ infobar/infobarpreferences/IBPRootListController.m
@@ -19,4 +19,12 @@
 - (void)openGithub {
 	[[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://github.com/naliferopoulos"]];
 }
+
+- (void)openTwitter {
+	NSURL *twitterURL = [NSURL URLWithString:@"twitter://user?screen_name=naliferopoulos"];
+	if ([[UIApplication sharedApplication] canOpenURL:twitterURL])
+		[[UIApplication sharedApplication] openURL:twitterURL];
+	else
+		[[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"http://www.twitter.com/naliferopoulos"]];
+}
 @end
